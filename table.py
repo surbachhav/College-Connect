5baf87bb975a1efb79bdb8e2b0680453408a5bae
divyshah123 <151473853+divyshah123@users.noreply.github.com>
2023-11-19 20:44:14 -0500

    Update table.py

--- table.py
+++ table.py
@@ -5,8 +5,8 @@ conn = psycopg2.connect("dbname=postgres user=postgres password=postgres")
 cur = conn.cursor()
 
 
-cur.execute("CREATE TABLE Colleges (College_Name text, State text, ACT_Avg int, SAT_Avg int, Enrollment int, Acceptance_Rate int, Cost int, Percent_Receiving_Aid int, National_Rank int, HS_Gpa float, Institutional_Control text )")
-cur.execute("INSERT INTO Colleges VALUES ('test', 'test', 1, 2, 3, 4, 5, 6, 7, 8, 'priv')")
+#cur.execute("CREATE TABLE Colleges (College_Name text, State text, ACT_Avg int, SAT_Avg int, Enrollment int, Acceptance_Rate int, Cost int, Percent_Receiving_Aid int, National_Rank int, HS_Gpa float, Institutional_Control text )")
+#cur.execute("INSERT INTO Colleges VALUES ('test', 'test', 1, 2, 3, 4, 5, 6, 7, 8, 'priv')")
 
 
 cur.close()
